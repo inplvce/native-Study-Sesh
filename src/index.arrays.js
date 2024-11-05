038a347d622ae95bed34feb6d1c6b929bbf1769b
ukaniushenka <inplvce@gmail.com>
2024-11-05 20:40:09 +0300

    array methods

--- src/index.arrays.js
+++ src/index.arrays.js
@@ -11,6 +11,13 @@ console.log(arr.newArr());
 
 // map
 
+//Вот задача:
+
+// У тебя есть массив чисел от 1 до 10.
+// Используй свой метод myMap, чтобы получить новый массив,
+// где каждый элемент — это квадрат числа
+// из исходного массива.
+
 Array.prototype.myMap = function (callback) {
   const result = new Array()
   for (let i = 0; i < this.length; i++) {
@@ -18,4 +25,4 @@ Array.prototype.myMap = function (callback) {
   }
   return result
 }
-console.log([1, 2, 3, 4, 5, 6].myMap(n => String(n)));
+console.log([1, 2, 3, 4, 5, 6, 7, 8, 9, 10].myMap(n => String(n*[n])));
