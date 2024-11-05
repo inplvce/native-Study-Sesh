91bffb7101fec333c087efce134d74fda86695a4
ukaniushenka <inplvce@gmail.com>
2024-11-05 08:59:35 +0300

    array methods

--- src/index.arrays.js
+++ src/index.arrays.js
@@ -4,5 +4,18 @@ console.log(arr);
 function newArr() {
   return `length array = ${this.length}`;
 }
+
 Array.prototype.newArr = newArr;
 console.log(arr.newArr());
+
+
+// map
+
+Array.prototype.myMap = function (callback) {
+  const result = new Array()
+  for (let i = 0; i < this.length; i++) {
+    result[i] = callback(this[i]);
+  }
+  return result
+}
+console.log([1, 2, 3, 4, 5, 6].myMap(n => String(n)));
