98293ba6b066dbd0ad1c63839870dcb98b22f7e2
ukaniushenka <inplvce@gmail.com>
2024-11-08 08:37:21 +0300

    events halding

--- src/index.events.js
+++ src/index.events.js
@@ -1,11 +1,14 @@
 const buttonClick = document.getElementById('clickMe')
 let clickCount = 0
 
-buttonClick.addEventListener('click', () => {
+buttonClick.addEventListener('click',  () => {
   clickCount++;
+  for (let i = 0; i < buttonClick; i++) {}
   if (clickCount === 1) {
     buttonClick.textContent = `Clicked ${clickCount} time`;
   } else {
     buttonClick.textContent = `Don’t click ${clickCount} times!!!`;
   }
-})
+}, {once: true})
+
+
